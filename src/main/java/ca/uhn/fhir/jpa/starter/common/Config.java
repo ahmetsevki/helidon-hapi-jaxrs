14c2c426026ab437f3d86a0462b66fd4a446661c
Ahmet Nalcacioglu <ahmet.nalcacioglu@oracle.com>
2023-10-30 18:23:40 -0400

    @Transactional create/update/read

--- src/main/java/ca/uhn/fhir/jpa/starter/common/Config.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/common/Config.java
@@ -7,14 +7,15 @@ import ca.uhn.fhir.jpa.starter.PersistenceContextProvider;
 import ca.uhn.fhir.jpa.util.IPersistenceContextProvider;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Default;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.persistence.EntityManager;
 
+
 public class Config {
-    @Inject
-    IPersistenceContextProvider persistenceContextProvider;
+
     @Produces
     @ApplicationScoped
     @Named("fhirContextR4")
@@ -23,9 +24,4 @@ public class Config {
         return FhirContext.forR4();
     }
 
-    @Produces
-    @RequestScoped
-    public EntityManager entityManager(){
-        return persistenceContextProvider.getEntityManager();
-    }
 }
